e269d3f59d7b85b4d4b1f26406fb34f38551d5b5
Shubham Parihar <shubhamparihar391@gmail.com>
2021-08-18 21:25:07 +0530

    feat: initial setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Counter />
+</template>
+
+<script setup>
+import Counter from './components/Counter.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='count'>{{ count }}</div>
+    <button @click='count--'>-</button>
+    <button @click='count++'>+</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<style scoped>
+.count {
+    font-size: 5em;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif
+}
+
+button {
+    font-size: 2em;
+    font-family: 'Courier New', Courier, monospace;
+    padding: 12px 20px;
+    margin: 5px;
+}
+</style>
